3bb14b5f31a7b22a9e42dba571734b8df47695db
jjnr27 <jhhchdh@gmail.com>
2017-11-21 15:59:56 -0700

    Listado parte 2

--- listado/index.js
+++ listado/index.js
@@ -3,20 +3,42 @@ const app = require('electron').app;
 const path = require('path');
 const url = require('url');
 const $ = require('jquery');
+function datos(nombre,genero,foto,direccion,telefono){
+  this.nombre    = nombre;
+  this.genero    = genero;
+  this.direccion = direccion;
+  this.foto      = foto;
+  this.telefono  = telefono;
+}
+var usuarios = new Array(20);
 function inicia(){
 $.ajax({
   url: 'https://randomuser.me/api/?results=20',
   dataType: 'json',
   success: function(data) {
-  	var resultado = "";
-  	var nombre = "";
+  	var resultado  = "";
+  	var nombre     = "";
+    var foto       = "";
+    var genero     = "";
+    var direccion  = "";
+    var telefono   = "";
   	for (var i = 0;i<20;i++) {
   		nombre= data.results[i].name.first+" "+data.results[i].name.last;
-  		resultado=resultado+"<li>"+nombre+"</li>";
-  		$("#Fotos").attr("src",data.results[i].picture.medium);
-  	}
-  	$("#lstUsuarios").append(resultado);
+      genero=data.results[i].gender;
+      direccion=data.results[i].location.street;
+      telefono=data.results[i].phone;
+      foto = data.results[i].picture.medium;
+  		resultado="<li><img src="+foto+">"+nombre+"<button id='"+i+"'>Detalle</button> </li>";
+  		$("#lstUsuarios").append(resultado);
+      usuarios[i]= new datos(nombre,genero,foto,direccion,telefono);
 
+  	}
+    function botonDetalle(){
+      alert(usuarios[this.id].nombre+" "+usuarios[this.id].genero+" "+usuarios[this.id].direccion);
+
+    }
+    $("body").on("click","li > button",botonDetalle);
+  	
     
   }
 });
